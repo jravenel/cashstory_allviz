1ae324a088c7abbb0194669b609509efae45b295
Thiago Sabetta <thiago.sabetta@toucantoco.com>
2017-03-16 14:28:02 +0100

    Creation talent-mapping

---
+++ talent-mapping/preprocess/augment.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+import pandas as pd
+import pdb
+
+def augment(dfs):
+    """
+    Insert here code to augment your data frames during preprocessing
+    """
+
+    # Add domain to each dataframe
+    for domain, df in dfs.iteritems():
+        df['domain'] = domain
+
+    return dfs
